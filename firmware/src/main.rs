4b07b240b6949e6747951ca44e75218ae75c4e7a
Ivor Wanders <ivor@iwanders.net>
2025-07-05 15:30:08 -0400

    This should be usb serial, but it doesn't work.

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -5,6 +5,9 @@
 //! Note: Without additional hardware, PC13 should not be used to drive an LED, see page 5.1.2 of
 //! the reference manual for an explanation. This is not an issue on the blue pill.
 
+// MCP9600; Default I2C address is 0x67.
+// Following the Start condition, the host must transmit an
+// 8-bit address byte to the MCP960X/L0X/RL0X
 #![deny(unsafe_code)]
 #![no_std]
 #![no_main]
@@ -13,10 +16,38 @@ use panic_halt as _;
 
 use nb::block;
 
+use cortex_m::asm::delay;
 use cortex_m_rt::entry;
-use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
+use stm32f1xx_hal::{pac, prelude::*, rcc, timer::Timer};
 
+use stm32f1xx_hal::usb::{Peripheral, UsbBus};
 
+use usb_device::prelude::*;
+use usbd_serial::{SerialPort, USB_CLASS_CDC};
+/*
+mod mcp9600 {
+    use embedded_hal::blocking::i2c::I2c;
+
+    const ADAFRUIT_MCP9600_ADDR: u8 = 0x67;
+    pub struct TemperatureSensorDriver<I2C> {
+        i2c: I2C,
+        address: u8,
+    }
+
+    impl<I2C: I2c> TemperatureSensorDriver<I2C> {
+        pub fn new(i2c: I2C, address: u8) -> Self {
+            Self { i2c, address }
+        }
+
+        pub fn read_device_id(&mut self) -> Result<u8, I2C::Error> {
+            let mut tmp = [0u8, 0u8];
+            self.i2c
+                .write_read(self.address as u8, &[0b0010_0000], &mut tmp)?;
+            Ok(tmp[1])
+        }
+    }
+}
+*/
 #[entry]
 fn main() -> ! {
     // Get access to the core peripherals from the cortex-m crate
@@ -31,23 +62,90 @@ fn main() -> ! {
 
     // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
     // `clocks`
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    // Set a real clock that allows usb.
+    let clocks = rcc
+        .cfgr
+        .use_hse(8.MHz())
+        .sysclk(48.MHz())
+        .pclk1(24.MHz())
+        .freeze(&mut flash.acr);
 
     // Acquire the GPIOC peripheral
     let mut gpioc = dp.GPIOC.split();
-
     // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
     // in order to configure the port. For pins 0-7, crl should be passed instead.
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
     // Configure the syst timer to trigger an update every second
+    /**/
     let mut timer = Timer::syst(cp.SYST, &clocks).counter_hz();
     timer.start(10.Hz()).unwrap();
 
     // Wait for the timer to trigger an update and change the state of the LED
+
+    assert!(clocks.usbclk_valid());
+
+    //---
+    //
+    let mut gpioa = dp.GPIOA.split();
+
+    // BluePill board has a pull-up resistor on the D+ line.
+    // Pull the D+ pin down to send a RESET condition to the USB bus.
+    // This forced reset is needed only for development, without it host
+    // will not reset your device when you upload new firmware.
+    let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
+    usb_dp.set_low();
+    delay(clocks.sysclk().raw() / 100);
+
+    let usb = Peripheral {
+        usb: dp.USB,
+        pin_dm: gpioa.pa11,
+        pin_dp: usb_dp.into_floating_input(&mut gpioa.crh),
+    };
+    let usb_bus = UsbBus::new(usb);
+
+    let mut serial = SerialPort::new(&usb_bus);
+    // ---
+
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .manufacturer("Fake company")
+        .product("Serial port")
+        .serial_number("TEST")
+        .device_class(USB_CLASS_CDC)
+        .build();
+
+    led.set_high();
     loop {
-        block!(timer.wait()).unwrap();
-        led.set_high();
-        block!(timer.wait()).unwrap();
-        led.set_low();
+        if !usb_dev.poll(&mut [&mut serial]) {
+            continue;
+        }
+        led.set_low(); // Turn on
+
+        let mut buf = [0u8; 64];
+
+        match serial.read(&mut buf) {
+            Ok(count) if count > 0 => {
+                led.set_low(); // Turn on
+
+                // Echo back in upper case
+                for c in buf[0..count].iter_mut() {
+                    if 0x61 <= *c && *c <= 0x7a {
+                        *c &= !0x20;
+                    }
+                }
+
+                let mut write_offset = 0;
+                while write_offset < count {
+                    match serial.write(&buf[write_offset..count]) {
+                        Ok(len) if len > 0 => {
+                            write_offset += len;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            _ => {}
+        }
+
+        led.set_high(); // Turn off
     }
 }
